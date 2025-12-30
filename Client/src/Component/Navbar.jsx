a20a3426da535e6f82496fca483fbb0f487f46d5
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2025-12-30 15:57:10 +0530

    structure ready👍

---
+++ Client/src/Component/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <>
+   <div>
+    <li>Pratik</li>
+    <li>Piyush</li>
+    <li>Devyani/steffi</li>
+    <li>Bhavna</li>
+    <li>Dhanasvi</li>
+    <li>Minal</li>
+    <li>Akash</li>
+   </div>
+   </>
+  )
+}
+
+export default Navbar
